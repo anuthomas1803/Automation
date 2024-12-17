6319c005f7479e6f04d31ff3e71ccf06aa8a4b81
anuth <anuth@192.168.0.6>
2024-12-18 00:13:56 +0530

    commit

---
+++ selenium/src/main/java/automation/Addition.java
@@ -0,0 +1,29 @@
+package automation;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+public class Addition extends BaseAuto {
+
+		public void twoinputFields()   
+		{
+		WebElement entervalueA=driver.findElement(By.xpath("//input[@id='value-a']"));
+		entervalueA.click();
+		entervalueA.sendKeys("25");
+		WebElement entervalueB=driver.findElement(By.xpath("//input[@id='value-b']"));
+		entervalueB.click();
+		entervalueB.sendKeys("100");
+		WebElement Gettotal=driver.findElement(By.xpath("//button[@id='button-two']"));
+		Gettotal.click();
+		}       
+		
+		public static void main(String[] args) {      
+			// TODO Auto-generated method stub
+			Addition messg=new Addition();
+			messg.initialiseBrowser();
+			messg.twoinputFields();
+		}
+
+	}
+
+
